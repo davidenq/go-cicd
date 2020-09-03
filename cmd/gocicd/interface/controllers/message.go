8178dce23cae741cca1b3c8815f089e5603a52d2
David <davip@live.com>
2020-09-03 12:33:12 -0500

    added the implementation of the second unit test

--- cmd/gocicd/interface/controllers/message.go
+++ cmd/gocicd/interface/controllers/message.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/davidenq/go-cicd/cmd/gocicd/domain"
+	"github.com/davidenq/go-cicd/cmd/gocicd/types"
+)
+
+//Handler .
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/text")
+	data, _ := ioutil.ReadAll(r.Body)
+	dataStruct := &types.BodyMessage{}
+	json.Unmarshal(data, &dataStruct)
+	message := domain.GenerateMessage(dataStruct)
+	fmt.Fprintf(w, message)
+}
--- cmd/gocicd/interface/controllers/message_test.go
+++ cmd/gocicd/interface/controllers/message_test.go
@@ -1,22 +1,32 @@
 package controllers
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/davidenq/go-cicd/cmd/gocicd/types"
 )
 
 func TestHandlerReturnsStringTypeOfData(t *testing.T) {
-	t.Run("should return a string type of data when receives a specific json data structure", func(t *testing.T) {
+	t.Run("should return: Hello Juan Perez your message will be send", func(t *testing.T) {
 		res := httptest.NewRecorder()
-		req, _ := http.NewRequest("POST", "http://localhost:4000/DevOps", nil)
+		data := &types.BodyMessage{
+			Message:       "This is a test",
+			To:            "Juan Perez",
+			From:          "Rita Asturia",
+			TimeToLifeSec: 45,
+		}
+		dataBytes, _ := json.Marshal(data)
+		req, _ := http.NewRequest("POST", "http://localhost:4000/DevOps", strings.NewReader(string(dataBytes)))
 		Handler(res, req)
-
 		var want string
 		got, _ := ioutil.ReadAll(res.Body)
-		if reflect.TypeOf(got) != reflect.TypeOf(want) {
+		if reflect.TypeOf(string(got)) != reflect.TypeOf(want) {
 			t.Errorf("got '%s' expected '%s", got, want)
 		}
 	})
