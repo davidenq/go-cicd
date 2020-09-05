764d24d7681ce6a1b72e308d0106d246bf4eec4a
David <davip@live.com>
2020-09-05 06:17:57 -0500

    using prefix + branch-name for deploy the app on google cloud run

--- scripts/app/app.sh
+++ scripts/app/app.sh
@@ -17,7 +17,7 @@ then
   if [ "$TRAVIS_BRANCH" == "" ]; then
     CURRENT_HOST=http://localhost:$PORT
   else
-    CURRENT_HOST=$(gcloud run services describe go-cicd-$TRAVIS_BRANCH-$TRAVIS_BUILD_ID  --platform managed --region us-east1 --format 'value(status.url)')
+    CURRENT_HOST=$(gcloud run services describe go-cicd-$TRAVIS_BRANCH  --platform managed --region us-east1 --format 'value(status.url)')
   fi
   HOST=$CURRENT_HOST go test ./tests/end2end
 fi
